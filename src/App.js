a4cb7979d3034dc61c1b7d5f4f19195c0d9120dd
Praful Aswale <prafulcode15@gmail.com>
2023-12-06 00:54:25 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { ToastContainer } from "react-toastify";
-// import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import NewsBoard from "./components/NewsBoard";
 
 function App() {
   return (
     <div>
       <ToastContainer />
-      <Signup />
+      <Navbar />
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route path="/articles" element={<NewsBoard />} />
+      </Routes>
     </div>
   );
 }
